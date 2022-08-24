0eb28b2d019efba34933fcf7ee05fe3fefa944e0
Keith Laster <lasterk9@gmail.com>
2022-08-23 21:26:48 -0500

    added test data to db

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,7 @@
 class ShowsController < ApplicationController
-  def index; end
+  def index
+    @shows = ['Boy Meets World', 'Stranger Things', 'Locke and Key']
+  end
 
   def show; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :ap_ranker do
-    resources :shows, only: %i[index show create]
-  end
+  get 'shows' => 'shows#index'
 end
